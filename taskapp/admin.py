fe99a6cdae26833427f06c040b752e8e6868b910
siobhain <sobrien@live.ie>
2023-08-17 21:43:42 +0000

    taskapp: Add ProfileAdmin with filter, search & actions to Admin Panel

--- taskapp/admin.py
+++ taskapp/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Profile, Task
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'surname', 'user')
+    list_filter = ('user', 'firstname', 'surname')
+    search_fields = ['firstname', 'surname', 'user']
+    actions = ['change_gate_code', 'new mobile']
+
+    def change_gate_code(self, request, queryset):
+        queryset.update(gate_code='2345')
+
+    def new_mobile(self, request, queryset):
+        queryset.update(mobile_number='1234567')    
+
 
 
 @admin.register(Task)
@@ -21,5 +33,6 @@ class TaskAdmin(admin.ModelAdmin):
     def complete_task(self, request, queryset):
         queryset.update(completed=True)
 
-    def change_to_new(self, request, queryset):
+# sob : change_to_new is used for testing purposes
+    def change_to_new(self, request, queryset):         
         queryset.update(status=False, completed=False)
