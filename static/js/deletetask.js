8f418817466e1f22fee7cd959612dd773d96e56b
siobhain <sobrien@live.ie>
2023-08-26 02:41:00 +0000

    taskapp: paleblue background, use modal & js to confirm delete task

--- static/js/deletetask.js
+++ static/js/deletetask.js
@@ -4,19 +4,31 @@ var deleteButtons = document.getElementsByClassName("btn-delete");
 var deleteConfirm = document.getElementById("deleteConfirm");
 // var commentText = document.getElementsByTagName("textarea")[0];
 // var commentForm = document.getElementById("commentForm");
-var submitButton = document.getElementById("submitButton");
+//var submitButton = document.getElementById("submitButton");
 
 // empty the comment text after post
 
 // commentText.value = "";
 
+// for (let button of deleteButtons) {
+//     button.addEventListener("click", (e) => {
+//         let taskId = e.target.getAttribute("mytaskid");
+        
+//         deleteConfirm.href = `/delete_task/${taskId}`;
+        
+//     });
+// }
+
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
-        let taskId = e.target.getAttribute("task_id");
-        deleteConfirm.href = `delete_task/${deleteId}`;
-        // deleteModal.show();
+      let taskId = e.target.getAttribute("mytaskid");
+      let deleteConfirm = document.getElementById("deleteConfirm");
+      $('#removeModal').on('show.bs.modal', function (event) {
+        deleteConfirm.href = `/delete_task/${taskId}`;
+      });
     });
 }
+  
 
 // for (let button of editButtons) {
 //     button.addEventListener("click", (e) => {
